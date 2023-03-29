a76348c041af7159ca37754d28c0485fdd6bd45e
jacob <114677726+InventiveFire@users.noreply.github.com>
2023-03-29 15:29:19 +0000

    Change to requests library

--- rockstarapi.py
+++ rockstarapi.py
@@ -1,5 +1,6 @@
 from genericpath import exists
-import urllib.request
+import json
+import requests
 
 class RockstarApi():
     FILTER = 0
@@ -22,11 +23,13 @@ class Rock():
         self.load()
 
     def load(self):
-        contents = urllib.request.urlopen(self.url).read()
+        #contents = urllib.request.urlopen(self.url).read()
+        contents = requests.get(self.url).json()
         
         
-
+        print (contents)
         #de-json it into dictionary
+
         self.contents = contents
         print (self.contents)
 
@@ -74,7 +77,13 @@ class Rock():
 rs = RockstarApi() # can take url default is "https://rockstarapi-production.up.railway.app/"
 rock=rs.getRock(0) # calls  https://rockstarapi-production.up.railway.app/rock/0 
 
+assert rock.contents == {"papa":[175,1533],"x":[2],"my_array":[{"0":"foo"},{"0":"foo","1":"bar"},{"0":"foo","1":"bar","2":"baz"},{"0":"foo","1":"bar","2":"baz","key":"value"}], "output":["Hello World",2,"foo","bar","baz","value",3]}
 assert rock.values() == {"papa":[175,1533],"x":[2],"my_array":[], "output":[2,3]}
+
+assert rock.get("papa") ==  [175,1533]
+assert rock.P("papa") ==  P([175,1533])
+assert rock.PBase("papa", 10) == P([1,7,5,1,5,3,3])
+
 
 rock.stringParse = RockstarApi.ROCKSTAR
 rock.load()
